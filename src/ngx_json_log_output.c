c177c8e19dce1c8a332c3ac9955b3e499a3eacdf
fooinha <fooinha@users.noreply.github.com>
2017-06-14 11:57:40 +0100

    fix/0.0.6/write file/log (#30)
    * ngx-json-log: more info while fail to log to file

--- src/ngx_json_log_output.c
+++ src/ngx_json_log_output.c
@@ -12,7 +12,9 @@ ngx_json_log_write_sink_file(ngx_log_t *log,
     len = strlen(txt);
     written = ngx_write_fd(fd, (u_char *)txt, len);
     if (len && len != written) {
-        ngx_log_error(NGX_LOG_EMERG, log, 0, "Mismatch size");
+        ngx_log_error(NGX_LOG_EMERG,
+                log, 0, "mismatch size: fd=%d len=%d written=%d",
+                fd, len, written);
         return NGX_ERROR;
     }
     return NGX_OK;
